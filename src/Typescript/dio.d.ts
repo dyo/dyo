ef4d0093404db1da095f507089161a6819980c32
Sultan Tarimo <sultantarimo@me.com>
2017-01-07 12:13:22 +0300

    Typescript definition d.ts file WIP

---
+++ src/Typescript/dio.d.ts
@@ -0,0 +1,79 @@
+declare enum nodeType {
+	Empty = 0,
+	Element = 1,
+	Component = 2,
+	Text = 3,
+	Fragment = 11
+}
+
+type Key = string | number;
+type Ref = string | Function;
+type Children = VNode[];
+type Type = string | Component<Props> | Function;
+
+interface Props {
+	children?: Children;
+	ref?: Ref;
+	key?: Key;
+}
+
+interface VNode {
+	nodeType: nodeType;
+	type: Type;
+	props: Props;
+	children: Children;
+	DOMNode: Node;
+	instance: Component<Props>;
+	index: number
+}
+
+interface VRequest {
+	method: string;
+	url: string;
+	payload: string | Object;
+	enctype: string;
+	responseType: string;
+	withCredentials: boolean;
+	headers: Object<string, any>;
+	initial: any;
+	config: (xhr: XMLHttpRequest) => void;
+	username: string;
+	password: string;
+}
+
+declare class Component <P> {
+	constructor (props?: P);
+	forceUpdate (callback?: () => void): void;
+	setState (state: any, callback?: () => void): void;
+	VNode?: VNode;
+	refs?: any;
+	state?: any;
+	props?: P;
+}
+
+// Components
+declare function createClass(shape: Function | Object): Component<Props>;
+declare function forceUpdate(callback: Function): void;
+declare function setState(state: Object<string, any>, callback: Function): void;
+
+// Elements
+declare function createElement(type: Type, props?: Props, ...children): VNode;
+declare function cloneElement(element: VNode, props: Props, children: Children): VNode;
+declare function isValidElement(any): boolean;
+declare function createFactory(type: Type, props: Props): createElement;
+declare function DOM(types: string[]): Object<string, createElement>;
+
+// Shape Factories
+declare function VElement(type: Type, props: Props, children: Children)
+declare function VComponent(type: Type, props: Props, children: Children)
+declare function VText(type: Type, props: Props, children: Children)
+declare function VSvg(type: Type, props: Props, children: Children)
+
+// Render
+declare function render(subject: Component | VNode | Object<string, any> | Function): (props: Props) => void;
+declare function shallow(subject: Component | VNode | Object<string, any> | Function): VNode;
+
+declare module 'dio' {
+	
+}
+
