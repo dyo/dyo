4412a6dbfa37235e3ded23aaf3c102132c7ce117
Sultan Tarimo <sultantarimo@me.com>
2017-05-17 14:37:35 +0300

    [7.0 BETA] cleanup error boundary

--- experiment/src/Boundary.js
+++ experiment/src/Boundary.js
@@ -10,7 +10,7 @@
 function dataBoundary (older, owner, type, props) {
 	try {
 		switch (type) {
-			case 0: return returnBoundary(older, owner.componentWillReceiveProps(props), owner, null, true);
+			case 0: returnBoundary(older, owner.componentWillReceiveProps(props), owner, null, true);
 			case 1: return owner.getInitialState(props);
 		}
 	} catch (err) {
@@ -32,8 +32,8 @@ function updateBoundary (older, owner, type, props, state) {
 	try {
 		switch (type) {
 			case 0: return owner.shouldComponentUpdate(props, state);
-			case 1: return returnBoundary(older, owner.componentWillUpdate(props, state), owner, null, true);
-			case 2: return returnBoundary(older, owner.componentDidUpdate(props, state), owner, null, false);
+			case 1: returnBoundary(older, owner.componentWillUpdate(props, state), owner, null, true);
+			case 2: returnBoundary(older, owner.componentDidUpdate(props, state), owner, null, false);
 		}
 	} catch (err) {
 		errorBoundary(err, older, owner, 1, type);
@@ -52,6 +52,7 @@ function renderBoundary (older, group) {
 		if (older.yield !== null) {
 			return older.yield();
 		}
+
 		switch (group) {
 			case FUNCTION: return older.type(older.props);
 			default: return older.owner.render(older.owner.props, older.owner.state);
@@ -72,8 +73,8 @@ function renderBoundary (older, group) {
 function mountBoundary (older, owner, node, type) {
 	try {
 		switch (type) {
-			case 0: return returnBoundary(older, owner.componentWillMount(node), owner, null, false);
-			case 1: return returnBoundary(older, owner.componentDidMount(node), owner, null, true);
+			case 0: returnBoundary(older, owner.componentWillMount(node), owner, null, false);
+			case 1: returnBoundary(older, owner.componentDidMount(node), owner, null, true);
 			case 2: return owner.componentWillUnmount(node);
 		}
 	} catch (err) {
@@ -95,7 +96,7 @@ function callbackBoundary (older, owner, callback, data, type) {
 		if (type === 0) {
 			return callback.call(owner, data);
 		} else {
-			return returnBoundary(older, callback.call(owner, data), owner, null, false);
+			returnBoundary(older, callback.call(owner, data), owner, null, false);
 		}
 	} catch (err) {
 		errorBoundary(err, older, owner, 2, callback);
@@ -165,12 +166,47 @@ function returnBoundary (older, state, owner, e, sync) {
  * @return {Tree?}
  */
 function errorBoundary (message, older, owner, type, from) {
-	var component = '#unknown';
-	var location;
+	var unknown = '#unknown';
+	var component = unknown;
+	var location = unknown;
 	var newer;
 
 	try {
-		location = errorLocation(type, from) || component;
+		if (typeof from === 'function') {
+			location = from.name;
+		} else {
+			switch (type) {
+				case 0: {
+					switch (from) {
+						case 0: location = 'componentWillReceiveProps';
+						case 1: location = 'getInitialState';
+					}
+					break;
+				}
+				case 1: {
+					switch (from) {
+						case 0: location = 'shouldComponentUpdate';
+						case 1: location = 'componentWillUpdate';
+						case 2: location = 'componentDidUpdate';
+					}
+					break;
+				}
+				case 3: {
+					return location = 'render';
+				}
+				case 4: {
+					switch (from) {
+						case 0: location = 'componentWillMount';
+						case 1: location = 'componentDidMount';
+						case 2: location = 'componentWillUnmount';
+					}
+					break;
+				}
+				case 5: {
+					return location = 'event';
+				}
+			}
+		}
 
 		if (owner !== null) {
 			if (owner.componentDidThrow !== void 0) {
@@ -184,7 +220,10 @@ function errorBoundary (message, older, owner, type, from) {
 		location = 'componentDidThrow';
 	}
 
-	errorMessage(component, location, message instanceof Error ? message.stack : message);
+	console.error(
+		(message instanceof Error ? message.stack : message) +
+		'\n\n  ^^ Error caught in '+'"'+component+'"'+' from "'+location+'" \n'
+	);
 
 	if (type === 3) {
 		if (newer === void 0 && older !== SHARED && older.node !== null) {
@@ -195,63 +234,4 @@ function errorBoundary (message, older, owner, type, from) {
 			return shape(newer, older, true);
 		}
 	}
-}
-
-/**
- * Error Location
- *
- * @param  {Number} type
- * @param  {Number|Function} from
- * @return {String?}
- */
-function errorLocation (type, from) {
-	if (typeof from === 'function') {
-		return from.name;
-	}
-
-	switch (type) {
-		case 0: {
-			switch (from) {
-				case 0: return 'componentWillReceiveProps';
-				case 1: return 'getInitialState';
-			}
-			break;
-		}
-		case 1: {
-			switch (from) {
-				case 0: return 'shouldComponentUpdate';
-				case 1: return 'componentWillUpdate';
-				case 2: return 'componentDidUpdate';
-			}
-			break;
-		}
-		case 3: {
-			return 'render';
-		}
-		case 4: {
-			switch (from) {
-				case 0: return 'componentWillMount';
-				case 1: return 'componentDidMount';
-				case 2: return 'componentWillUnmount';
-			}
-			break;
-		}
-		case 5: {
-			return 'event';
-		}
-	}
-}
-
-/**
- * Error Message
- *
- * @param  {String} component
- * @param  {String} location
- * @param  {String} message
- */
-function errorMessage (component, location, message) {
-	console.error(
-		message + '\n\n  ^^ Error caught in ' + '"' + component + '"' +
-		' from "' + location + '" \n'
-	);
 }
