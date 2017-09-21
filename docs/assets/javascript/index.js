de24508b1a617e1e71e1823141559ccb54dfd4c7
Sultan Tarimo <sultantarimo@me.com>
2017-09-21 17:30:17 +0300

    [docs] improve style &  include hash links

--- docs/assets/javascript/index.js
+++ docs/assets/javascript/index.js
@@ -1,20 +1,18 @@
-(function (){
-	var selector = 'code';
-	var comments = 'comment';
-	var keys = 'key';
-	var newline = 'newline';
-	var words = 'word';
-	var number = 'number';
-	var operators = 'operator';
-	var regex = 'regex';
-	var space = 'space';
-	var string = 'string';
-	var unknown = 'unknown';
-	var prefix = 'syntax-';
-	var id = 'highlight-style';
+!function () {
+	var selector = 'code'
+	var comments = 'comment'
+	var keys = 'key'
+	var newline = 'newline'
+	var words = 'word'
+	var number = 'number'
+	var operators = 'operator'
+	var regex = 'regex'
+	var space = 'space'
+	var string = 'string'
+	var unknown = 'unknown'
+	var prefix = 'syntax-'
 
-	var RESERVED_KEYS = /^(a(bstract|lias|nd|rguments|rray|s(m|sert)?|uto)|b(ase|egin|ool(ean)?|reak|yte)|c(ase|atch|har|hecked|lass|lone|ompl|onst|ontinue)|document|de(bugger|cimal|clare|f(ault|er)?|init|l(egate|ete)?)|do|double|e(cho|ls?if|lse(if)?|nd|nsure|num|vent|x(cept|ec|p(licit|ort)|te(nds|nsion|rn)))|f(allthrough|alse|inal(ly)?|ixed|loat|or(each)?|riend|rom|unc(tion)?)|global|goto|guard|i(f|mp(lements|licit|ort)|n(it|clude(_once)?|line|out|stanceof|t(erface|ernal)?)?|s)|l(ambda|et|ock|ong)|m(odule|utable)|NaN|n(amespace|ative|ext|ew|il|ot|ull)|o(bject|perator|r|ut|verride)|p(ackage|arams|rivate|rotected|rotocol|ublic)|r(aise|e(adonly|do|f|gister|peat|quire(_once)?|scue|strict|try|turn))|s(byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch)|t(emplate|hen|his|hrows?|ransient|rue|ry|ype(alias|def|id|name|of))|u(n(checked|def(ined)?|ion|less|signed|til)|se|sing)|v(ar|irtual|oid|olatile)|w(char_t|hen|here|hile|ith)|xor|yield)$/;
-	var RESERVED_TOKENS = [
+	var tokens = [
 		[number, /#[0-9a-f]{6}\b/],
 		[number, /#[0-9a-f]{3}\b/],
 		[comments, /\/\/.*?(?=\n|$)/],
@@ -31,78 +29,99 @@
 		[space, /[^\S\n]+/],
 		[words, /[\w$]+/],
 		[unknown, /./]
-	];
-
-	var styleContent = [
-		'word{color:#444',
-		'operator{color:#444',
-		'number{color:#1C00CF',
-		'regex{color:#C41A16',
-		'key{color:#AA0C91',
-		'string{color:#C41A16',
-		'comment{color:#007400'
 	]
-	.map(function (value) { return '.' + prefix + value + ';}' })
-	.join('') + selector + '{font:0.9em/1.618 Consolas, "Liberation Mono", Menlo, Courier, monospace;tab-size:2;}';
+	
+	var keywords = '^(\
+		a(bstract|lias|nd|rguments|rray|s(m|sert)?|uto)|\
+		b(ase|egin|ool(ean)?|reak|yte)|\
+		c(ase|atch|har|hecked|lass|lone|ompl|onst|ontinue)|\
+		document|\
+		de(bugger|cimal|clare|f(ault|er)?|\
+		init|l(egate|ete)?)|\
+		do|double|\
+		e(cho|ls?if|lse(if)?|nd|nsure|num|vent|x(cept|ec|p(licit|ort)|te(nds|nsion|rn)))|\
+		f(allthrough|alse|inal(ly)?|ixed|loat|or(each)?|riend|rom|unc(tion)?)|\
+		global|goto|guard|\
+		i(f|mp(lements|licit|ort)|n(it|clude(_once)?|line|out|stanceof|t(erface|ernal)?)?|s)|\
+		l(ambda|et|ock|ong)|m(odule|utable)|\
+		NaN|\
+		n(amespace|ative|ext|ew|il|ot|ull)|\
+		o(bject|perator|r|ut|verride)|\
+		p(ackage|arams|rivate|rotected|rotocol|ublic)|\
+		r(aise|e(adonly|do|f|gister|peat|quire(_once)?|scue|strict|try|turn))|\
+		s(byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch)|\
+		t(emplate|hen|his|hrows?|ransient|rue|ry|ype(alias|def|id|name|of))|\
+		u(n(checked|def(ined)?|\ion|less|signed|til)|se|sing)|\
+		v(ar|irtual|oid|olatile)|\
+		w(char_t|hen|here|hile|ith)|xor|yield\
+	)$'.replace(/\s/g, '')
 
-	style = document.createElement('style');
-	style.textContent = styleContent;
-	style.id = id;
-
-	function tokenize (text) {
-		var tokens = [];
-		var length = RESERVED_TOKENS.length;
-		var forceDiv = false;
-		var className;
-		var token;
-		var matches;
+	var regex = new RegExp(keywords, 'g')
+	var font = selector + '{font:0.9em/1.618 Consolas, "Liberation Mono", Menlo, Courier, monospace;tab-size:2;}'
+	var style = [
+		'.syntax-word{color:#444}',
+		'.syntax-operator{color:#444}',
+		'.syntax-number{color:#1C00CF}',
+		'.syntax-regex{color:#C41A16}',
+		'.syntax-key{color:#AA0C91}',
+		'.syntax-string{color:#C41A16}',
+		'.syntax-comment{color:#007400}',
+		font
+	]
+	
+	var tokenize = function (text) {
+		var stack = []
+		var split = false
 
 		while (text) {
-			for (var index = 0; index < length; index += 1) {
-				matches = RESERVED_TOKENS[index][1].exec(text);
+			for (var i = 0; i < tokens.length; ++i) {
+				var matches, type, value
 
-				if (!matches || matches.index !== 0) continue;
+				if (!(matches = tokens[i][1].exec(text)) || matches.index !== 0)
+					continue
 
-				className = RESERVED_TOKENS[index][0];
+				if ((type = tokens[i][0]) === regex && split)
+					continue
 
-				if (className === regex && forceDiv) continue;
+				if (regex.test((value = matches[0])) && type === words) 
+					type = keys
 
-				token = matches[0];
+				if (type !== space) 
+					split = type === number || type === words
 
-				if (className === words && RESERVED_KEYS.test(token)) className = keys;
-				if (className !== space) forceDiv = className === number || className === words;
+				text = text.slice(value.length)
+				stack.push([type, value])
 
-				text = text.slice(token.length);
-				tokens.push([className, token]);
-				break;
+				break
 			}
 		}
 
-		return tokens;
+		return stack
 	}
 
-	function highlighter (element) {
-		var tokens = tokenize(element.textContent.trim());
-		element.innerHTML = '';
+	var collection = [].slice.call(document.querySelectorAll(selector))
 
-		tokens.forEach(function (token) {
-			var tokenElement = document.createElement('span');
-			tokenElement.className = prefix + token[0];
-			tokenElement.textContent = token[1];
-			element.appendChild(tokenElement);
-		});
-	}
+	collection.forEach(function (node) {
+		var stack = tokenize(node.textContent.trim())
+		
+		node.textContent = ''
 
+		stack.forEach(function (type) {
+			var span = document.createElement('span')
+			span.className = prefix + type[0]
+			span.textContent = type[1]
+			node.appendChild(span)
+		})
+	})
 
-	function highlight () {
-		if (!document.getElementById(id)) {
-			document.head.appendChild(style);
-		}
+	document.head.appendChild(document.createElement('style')).textContent = style.join('')
+}()
 
-		Array.prototype.slice.call(document.querySelectorAll(selector)).forEach(function (element) {
-			highlighter(element);
-		});
-	}
+!function () {
+	[].slice.call(document.querySelectorAll('h1,h2')).forEach(function (node) {
+		if (!node.id)
+			return
 
-	highlight()
-}());
+		node.innerHTML = '<a href=#'+node.id+'>'+node.textContent+'</a>'
+	})
+}()
