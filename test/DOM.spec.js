7931fdd4194ec2490bce0c6aab602929b8832288
Sultan Tarimo <sultantarimo@me.com>
2017-09-26 22:55:47 +0300

    forgot to add DOM spec

---
+++ test/DOM.spec.js
@@ -0,0 +1,74 @@
+describe('DOM', () => {
+	it('should findDOMNode from component', () => {
+		let container = document.createElement('div')
+		let refs = null
+
+		render(class {
+			componentDidMount() {
+				refs = findDOMNode(this)
+			}
+			render() {
+				return h('div', h('span', 1))
+			}
+		}, container)
+
+		assert.html(refs, '<span>1</span>')
+	})
+
+	it('should findDOMNode from element', () => {
+		let container = document.createElement('div')
+		let refs = null
+		let element = null
+
+		render(class {
+			componentDidMount() {
+				refs = findDOMNode(element)
+			}
+			render() {
+				return element = h('div', h('span', 1))
+			}
+		}, container)
+
+		assert.html(refs, '<span>1</span>')
+	})
+
+	it('should findDOMNode from DOM node', () => {
+		let container = document.createElement('div')
+		let refs = null
+		let element = null
+
+		render(class {
+			componentDidMount(node) {
+				refs = findDOMNode(node)
+			}
+			render() {
+				return h('div', h('span', 1))
+			}
+		}, container)
+
+		assert.html(refs, '<span>1</span>')
+	})
+
+	it('should not findDOMNode from falsey value', () => {
+		assert.throws(() => {
+			findDOMNode(null)
+		})
+	})
+
+	it('should not findDOMNode from unmount component', () => {
+		assert.throws(() => {
+			findDOMNode(h('div'))
+		})
+	})
+
+	it('should findDOMNode from event', () => {
+		let event = new Event('click')
+		let node = document.createElement('div')
+		
+		Object.defineProperty(event, 'currentTarget', {value: node})
+
+		assert.doesNotThrow(() => {
+			assert.equal(findDOMNode(event), node)
+		})
+	})
+})
