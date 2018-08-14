99dad6ec07b353521e36cc26bd19d11b1ccdada1
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2018-08-14 13:14:28 +0200

    #76 - modified packages paths in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'development') {
-  module.exports = require('./dist/dio.cjs.development.min.js')
+  module.exports = require('./dist/dio.umd.development.min.js')
 } else {
-  module.exports = require('./dist/dio.cjs.production.min.js')
+  module.exports = require('./dist/dio.umd.production.min.js')
 }
