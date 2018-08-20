bbfef5b43ddf601bab0822152ebbb9afe0255643
Sultan Tarimo <sultantarimo@me.com>
2018-08-20 05:16:29 +0300

    rollup-ify

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'development') {
-  module.exports = require('./dist/dio.cjs.development.min.js')
-} else {
-  module.exports = require('./dist/dio.cjs.production.min.js')
-}
